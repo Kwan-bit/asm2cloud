18929f7b72505f73b5cbc55fe6449b2c43db68a1
Kwan-bit <66770115+Kwan-bit@users.noreply.github.com>
2021-03-02 15:07:14 +0700

    Add files via upload

---
+++ shoptest/add.php
@@ -0,0 +1,86 @@
+<?php
+      $host_heroku = "ec2-54-146-73-98.compute-1.amazonaws.com";
+      $db_heroku = "d14vh8umn11kov";
+      $user_heroku = "npcoqeoyfrccgh";
+      $pw_heroku = "a839d30a192380c4bfa7d14f6e75a3601e2ac16af1e81b34a2e05e1a77fd5737";
+      $conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
+      $pg_heroku = pg_connect($conn_string);
+if (!$pg_heroku)
+			{
+				die('Error: Could not connect: ' . pg_last_error());
+			}
+?>
+<html>
+  <head>
+    <link rel="stylesheet" href ="styles.css">
+    <title> Add </title>
+    <marquee bgcolor="gray" 
+             scrollamount="12" 
+             direction="left" 
+             onmouseover="this.stop();" 
+             onmouseout="this.start();">
+        [2/27/2021]: Remember to check the database everyday!
+    </marquee>
+  </head>
+  <body>
+	<style>
+        body {
+          background-image: url('background.jpg');
+          background-attachment: fixed;
+          background-size: 100%100%;
+        } 
+      	</style>
+    <br>
+    <form action="" method="GET">
+      <table border"0" bgcolor="white" align="center" cellspacing="20">
+        
+        <tr>
+          <td>Product ID</td>
+          <td><input type="text" value="" name="product_id" required></td>
+        </tr>
+        
+        <tr>
+          <td>Product Name</td>
+          <td><input type="text" value="" name="product_name" required></td>
+        </tr>
+        
+        <tr>
+          <td>Product Price</td>
+          <td><input type="text" value="" name="product_price" required></td>
+        </tr>
+        
+        <tr>
+          <td>Stock</td>
+          <td><input type="text" value="" name="product_stock" required></td>
+        </tr>
+        
+        <tr>
+          <td colspan="2" align="center"><input type="submit" id="button" name="submit" value="Add"></td>
+        </tr>
+    </form>
+  </table>
+</body>
+</html>
+
+<?php
+if($_GET['submit'])
+{
+	$pi=$_GET['product_id'];
+	$pn=$_GET['product_name'];
+	$pp=$_GET['product_price'];
+	$ps=$_GET['product_stock'];
+	$query = "INSERT INTO product VALUES ('$pi','$pn','$pp','$ps')";
+	$data = pg_query($pg_heroku,$query);
+	if($data)
+	{
+		echo "<script>alert('Added Successfully!')</script>";
+		?>
+		<meta http-equiv="refresh" content="0; url=https://asm2kwan.herokuapp.com/staff.php" />
+		<?php
+	}
+	else
+	{
+		echo "Failed to add product to the table.";
+	}
+}
+?>
---
+++ shoptest/main.php
@@ -0,0 +1,65 @@
+
+<html>
+<head>
+    <link rel="stylesheet" href ="styles.css">
+    <title> Login </title>
+    <marquee bgcolor="gray" 
+             scrollamount="12" 
+             direction="left" 
+             onmouseover="this.stop();" 
+             onmouseout="this.start();">
+        [nah/nah/nah]: nah
+    </marquee>
+    <ul> 
+        <li> <a href="index.php">Main Page</a></li>
+        <li> <a href="">Login </a></li>       
+    </ul>
+  </head>
+    <body>
+    <div class="container">
+      <img src="../static/image.png" class="image" />
+      <form action="/submit" method="POST">
+        <div class="form-group">
+          <h3>Customer Name</h3>
+          <input
+            type="text"
+            name="customer"
+            placeholder="Full name of customer"
+          />
+        </div>
+        <div class="form-group">
+          <h3>order</h3>
+          <select name="order">
+            <option value="">Select goods</option>
+            <option value="Oak wood table">Oak wood table</option>
+            <option value="Bookcases">Bookcases</option>
+            <option value="Settees">Settees</option>
+            <option value="Leather sofa">Leather sofa</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <h3>Please chose your order size</h3>
+          <p></p><input type="radio" name="size" value="1" /> 60x38 (sofa)</p>
+          <p></p><input type="radio" name="size" value="2" /> 42x40 (sofa)</p>
+          <p></p><input type="radio" name="size" value="3" /> 108x38 and 78x36 (sofa)</p>
+          <p></p><input type="radio" name="size" value="4" /> 84x36 (sofa)</p>
+          <p></p><input type="radio" name="size" value="5" /> 72x40 (table)</p>
+          <p></p><input type="radio" name="size" value="6" /> 42x24 (table)</p>
+          <p></p><input type="radio" name="size" value="7" checked /> 50x62 (bookcases)</p>
+          <img src="../static/image2.png" class="image2" style="width:462px;height:305px;"/>
+        </div>
+        <div class="form-group">
+          <h3>Customer note:</h3>
+          <textarea
+            name="comments"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Tell us if you have any specific order for your order:"
+          ></textarea>
+        </div>
+        <input type="submit" value="Submit" class="btn" />
+      </form>
+    </div>
+  </body>
+</html>
